497ea99d5c5250fd38d2be18617bf01c8b5294f9
Scudd <scudway.rbih1995@gmail.com>
2018-03-09 15:07:11 +0100

    create a check for leap year method

--- calculate.py
+++ calculate.py
@@ -1,1 +1,12 @@
 daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def is_leap_year(year):
+    if year % 4 != 0:
+        return False
+    elif year % 400 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
